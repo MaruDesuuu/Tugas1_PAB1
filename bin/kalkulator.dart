a2e9788d5e7a002d3bbb348328ee396c91b026d4
Mario Stevanus <126873119+MaruDesuuu@users.noreply.github.com>
2023-10-14 20:24:05 +0700

    Revert "Ricky Elsaputra"

--- bin/kalkulator.dart
+++ bin/kalkulator.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 class Kalkulator {
   var _bil1;
   var _bil2;
-  var _pilihan;
 
   double setBil1() {
     print("Masukkan Bilangan 1 : ");
@@ -13,16 +12,5 @@ class Kalkulator {
   double setBil2() {
     print("Masukkan Bilangan 2 : ");
     return _bil2 = double.parse(stdin.readLineSync()!);
-  }
-
-  double setPilihan() {
-    print("[1] Tambah");
-    print("[2] Kurang");
-    print("[3] Kali");
-    print("[4] Bagi");
-    print("Pilihan Anda :");
-    _pilihan = double.parse(stdin.readLineSync()!);
-    print(_pilihan);
-    return _pilihan;
   }
 }
