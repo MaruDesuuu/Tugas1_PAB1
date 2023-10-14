c1f2f025c9cb61254da7484bba88aba4d31d313b
dannielrww <dankuro121@gmail.com>
2023-10-14 21:30:30 +0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,3 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  var kondisi = "Y";
-  while (kondisi == "Y") {
-    var ulang = true;
-    print("Apakah anda ingin mengulang perhitungan ? [Y/N]");
-    kondisi = stdin.readLineSync()!.toUpperCase();
-    if (kondisi.toUpperCase() == "Y") {
-      ulang = false;
-    } else if (kondisi.toUpperCase() == "N") {
-      ulang = false;
-      break;
-    }
-  }
-}
+void main(List<String> arguments) {}
