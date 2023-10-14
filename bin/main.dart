8b56839cbe7ba37d46f78a6619aba0bdbbb3f0c3
renaldo0325 <renaldo75joker@gmail.com>
2023-10-14 21:41:26 +0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -5,22 +5,26 @@ void main(List<String> arguments) {
   var kondisi = "Y";
   while (kondisi == "Y") {
     Kalkulator kal1 = Kalkulator();
-
-    kal1.setBil1();
-    kal1.setBil2();
-    kal1.setPilihan();
-    kal1.hitung();
-    kal1.getHasil();
-
-    bool ulang = true;
-    while (ulang == true) {
-      print("Apakah anda ingin mengulang perhitungan ? [Y/N]");
-      kondisi = stdin.readLineSync()!.toUpperCase();
-      if (kondisi.toUpperCase() == "Y") {
-        ulang = false;
-      } else if (kondisi.toUpperCase() == "N") {
-        ulang = false;
-        break;
+    try {
+      kal1.setBil1();
+      kal1.setBil2();
+      kal1.setPilihan();
+      kal1.hitung();
+      kal1.getHasil();
+    } catch (e) {
+      print(
+          "Pilihan anda tidak terdaftar, mohong masukkan pilihan yang terdaftar !");
+    } finally {
+      bool ulang = true;
+      while (ulang == true) {
+        print("Apakah anda ingin mengulang perhitungan ? [Y/N]");
+        kondisi = stdin.readLineSync()!.toUpperCase();
+        if (kondisi.toUpperCase() == "Y") {
+          ulang = false;
+        } else if (kondisi.toUpperCase() == "N") {
+          ulang = false;
+          break;
+        }
       }
     }
   }
