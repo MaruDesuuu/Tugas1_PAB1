4267922d7d67803eab968f8242cd03bffd3f307e
dannielrww <dankuro121@gmail.com>
2023-10-14 21:29:47 +0700

    Looping

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,16 @@
 import 'dart:io';
-import 'kalkulator.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var kondisi = "Y";
+  while (kondisi == "Y") {
+    var ulang = true;
+    print("Apakah anda ingin mengulang perhitungan ? [Y/N]");
+    kondisi = stdin.readLineSync()!.toUpperCase();
+    if (kondisi.toUpperCase() == "Y") {
+      ulang = false;
+    } else if (kondisi.toUpperCase() == "N") {
+      ulang = false;
+      break;
+    }
+  }
+}
